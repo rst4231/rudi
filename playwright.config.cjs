57359021d47d2a16f391ef67d80785ca09bae5ac
rst4231 <cpateammail@gmail.com>
2026-09-21 16:29:15 +0300

    test: run iPhone viewport coverage in Chromium

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -9,6 +9,7 @@ module.exports=defineConfig({
   use:{
     baseURL:'http://127.0.0.1:4173',
     ...devices['iPhone 13'],
+    browserName:'chromium',
     locale:'ru-RU',
     timezoneId:'Europe/Moscow',
     colorScheme:'dark',
